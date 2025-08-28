eff9139c18673c90dff01980d4cc1ef5292bab93
johan S <johansuarez686@gmail.com>
2025-08-28 13:07:15 -0500

    Actividad

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("holaa");
 
+}public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("1 GIT STATUS - ESTADO RAMA");
+        System.out.println("2 GIT ADD . - AGREGAR ARCHIVOS");
+        System.out.println("3 GIT COMMIT -M GUARDAR LOCAL");
+        System.out.println("4 GIT PUSH ORIGIN - SUBIR CAMBIOS");
+        System.out.println("5 GIT CLONE - CLONAR PROYECTO");
+        System.out.println("6 GIT BRANCH - VER LAS RAMAS");
+        System.out.println("7 GIT FETCH - RAMAS REMOTAS");
+        System.out.println("8 GIT SWITCH -C - CREAR Y POSICIONARME SOBRE LA RAMA");
+        //ACTIVIDAD
+        //rama con nombre
+        System.out.println("Nombre Completo: Johan Andres Suarez Machete");
+        System.out.println("Lugar donde vivo: Zipaquira, Cundinamarca");
+        System.out.println("Mascotas: Si, tengo un perro");
     }
 }
